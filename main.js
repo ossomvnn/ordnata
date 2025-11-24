05645490d701ba4ac636149b66985a0fc78926d7
ossomvnn <ossomvn@outlook.com>
2025-11-24 11:12:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path');
 function createWindow() {
     // Erstellt das Browser-Fenster mit den definierten Maßen und Web-Einstellungen.
     const win = new BrowserWindow({
-        width: 1100, // Ihre gewünschte Breite
-        height: 750, // Ihre gewünschte Höhe
+        width: 1100,
+        height: 750,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'), 
             nodeIntegration: true,
